5386e355dd7a3c74b4a363ff6d63581032fe9e4e
Raditya Dwi Pahlawadi <radeed.71@gmail.com>
2026-05-26 20:58:44 +0700

    update 7

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        posDocumentation: resolve(__dirname, 'pos-documentation.html'),
+      },
+    },
+  },
 })
